ca05d1996218d1af98edb50e5c2bb96e24e9c3e6
NazikHarper <nazarolejnik58@gmail.com>
2022-02-17 20:44:52 +0200

    HW39

---
+++ main.js
@@ -0,0 +1,42 @@
+
+                                                    //ДЗ
+function display_message(){
+    alert("люблю шаурму без мяса")
+}
+display_message()
+
+
+// let title = "Гарри Поттер"
+// function favorite_book(title){
+//     alert(`One of my favorite book is ${title}`)
+// }
+// favorite_book(title)
+
+// let size = prompt("Введите размер футболки (S,M,L,XL).")
+// let text =prompt("Введите текст футболки")
+// function make_shirt(size, text){
+//     alert(`Вы выбрали футболку с размером ${size} и текстом "${text}".`)
+// }
+// make_shirt(size, text)
+
+
+// function make_shirt(a, b){
+//     let size = "L"
+//     let text = "I love JS"
+//     alert(`Вы выбрали футболку с размером ${size} и текстом "${text}".`)
+// }
+// let a = prompt("Введите размер футболки (S,M,L,XL).")
+// let b =prompt("Введите текст футболки")
+// make_shirt(a, b)
+// alert(`Вы выбрали футболку с размером ${a} и текстом "${b}".`)
+
+// let c = prompt("Введите название города")
+// function describe_city(a){
+//     let count = "Украина"
+//     alert(`${c} это ${count}`)
+// }
+// let a = prompt("Введите название страны")
+// alert(`${c} это ${a}`)
+// describe_city(a)
+// describe_city(a)
+// describe_city(a)
